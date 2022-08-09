5ecde25984548d725a4be4bdf02802c0dcafed41
exe-walt <106749659+exe-walt@users.noreply.github.com>
2022-08-09 22:54:15 +0100

    Update printf_srev.c

--- printf_srev.c
+++ printf_srev.c
@@ -13,6 +13,7 @@ int printf_srev(va_list args)
 	int i;
 	int j = 0;
 
+	
 	if (s == NULL)
 		s = "(null)";
 	while (s[j] != '\0')
@@ -20,4 +21,5 @@ int printf_srev(va_list args)
 	for (i = j - 1; i >= 0; i--)
 		_putchar(s[i]);
 	return (j);
+
 }
