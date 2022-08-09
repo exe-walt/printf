e64297e0f801da13865bcb1a465587e6c0703a06
exe-walt <106749659+exe-walt@users.noreply.github.com>
2022-08-09 22:55:42 +0100

    Update printf_string.c

--- printf_string.c
+++ printf_string.c
@@ -12,6 +12,7 @@ int printf_string(va_list val)
 
 	s = va_arg(val, char *);
 	if (s == NULL)
+	
 	{
 		s = "(null)";
 		len = _strlen(s);
@@ -26,4 +27,5 @@ int printf_string(va_list val)
 			_putchar(s[i]);
 		return (len);
 	}
+
 }
